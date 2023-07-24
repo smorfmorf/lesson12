51858bc96029eb13365c1c6583cb6aee1f5104cd
max <malalamobilelegends@gmail.com>
2023-07-25 02:46:11 +1000

    ''

--- game.js
+++ game.js
@@ -2,27 +2,27 @@ function playGame() {
     let secretNumber;
     let playing = true;
 
-    while (playing) {
-        secretNumber = Math.floor(Math.random() * 5);
-        let userInput = prompt(
-            "Угадай число от 1 до 5 или нажми 'Отмена', чтобы выйти:"
-        );
+    secretNumber = Math.floor(Math.random() * 5);
+    let userInput = prompt(
+        "Угадай число от 1 до 5 или нажми 'Отмена', чтобы выйти:"
+    );
 
-        if (userInput === null) {
-            alert("Игра окончена.");
-            playing = false; // Выход из цикла
+    if (userInput === null) {
+        alert("Игра окончена.");
+        playing = false; // Выход из цикла
+    } else {
+        let guess = +userInput;
+        if (isNaN(guess)) {
+            alert("Введи число!");
+            playGame();
+        } else if (guess > secretNumber) {
+            alert("Меньше!");
+            playGame();
+        } else if (guess < secretNumber) {
+            alert("Больше!");
+            playGame();
         } else {
-            let guess = +userInput;
-            if (isNaN(guess)) {
-                alert("Введи число!");
-            } else if (guess > secretNumber) {
-                alert("Меньше!");
-            } else if (guess < secretNumber) {
-                alert("Больше!");
-            } else {
-                alert("Правильно!");
-                playing = false; // Выход из цикла
-            }
+            alert("Правильно!");
         }
     }
 }
